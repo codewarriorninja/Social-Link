783236e691761b9f7a5909c382efd66cb5481dd8
Henok <henokaragaw17@gmail.com>
2025-04-02 00:09:36 -0900

    minor update

--- components/UnAuthenticatedSidebar.tsx
+++ components/UnAuthenticatedSidebar.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button"
 
 const UnAuthenticatedSidebar = () => {
   return (
-    <div className="sticky top-20">
+    <div className="sticky top-0">
       <Card>
         <CardHeader>
           <CardTitle className="text-center text-xl font-semibold">Welcome Back</CardTitle>
@@ -13,12 +13,12 @@ const UnAuthenticatedSidebar = () => {
         <CardContent>
           <p className="text-center text-muted-foreground mb-4">Login to access your profile and connect with other</p>
           <SignInButton mode="modal">
-            <Button className="w-full mt-2" variant={'default'}>
+            <Button className="w-full mt-2 cursor-pointer" variant={'default'}>
                 Log In
             </Button>
           </SignInButton>
           <SignUpButton mode="modal">
-            <Button className="w-full mt-2" variant={'default'}>
+            <Button className="w-full mt-2 cursor-pointer" variant={'default'}>
                 Sign Up
             </Button>
           </SignUpButton>
